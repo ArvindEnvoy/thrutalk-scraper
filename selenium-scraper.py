50848752966aaa9ffa0526960a47bb1c91878845
ArvindEnvoy <arvind@envoy.com>
2020-10-14 10:45:35 -0700

    adding other services and per date logic

--- selenium-scraper.py
+++ selenium-scraper.py
@@ -1,18 +1,12 @@
-from selenium import webdriver
-
-# from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
-
-# import civis
 import time
 import datetime
-from bs4 import BeautifulSoup
-import gspread_dataframe as gd
 import gspread as gs
 
-# Civis API
-# client = civis.APIClient()
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from bs4 import BeautifulSoup
+from gspread.exceptions import WorksheetNotFound
 
 # Set Selenium driver options
 options = webdriver.ChromeOptions()
@@ -22,22 +16,26 @@ options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--headless")
 
-# Create an empty df
-cols = ["Status", "Email", "Phone", "Livevox Login", "Service"]
-df = pd.DataFrame(columns=cols)
-
-
 # Start chrome diver
 driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
 
 # Define our ThruTalk Services
-services = ["fl_dem2020victory_miamidade"]
+services = {
+    "fl_dem2020victory_distributed": "Distributed",
+    "fl_dem2020victory_east_central": "East central",
+    "fl_dem2020victory_miamidade": "Miami Dade",
+    "fl_dem2020victory_north": "North",
+    "fl_dem2020victory_south": "South",
+    "fl_dem2020victory_southwest": "Southwest",
+    "fl_dem2020victory_spanish": "Spanish",
+    "fl_dem2020victory_team": "Team",
+    "fl_dem2020victory_west_central": "West central",
+}
 
-for service in services:
-    # Access desired URL
-    # https://metrics.thrutalk.io/agent-status/fl_dem2020victory_miamidade_callers
+
+def get_agent_status_data(service):
+    # url = "https://metrics.thrutalk.io/agent-status/fl_dem2020victory_distributed_callers"
     url = "https://metrics.thrutalk.io/agent-status/" + service + "_callers"
-
     driver.get(url)
 
     # Wait a sec
@@ -49,70 +47,73 @@ for service in services:
 
     # Get table body
     table_body = soup.find_all("tbody")
-
     if len(table_body) > 0:
         table_rows = table_body[0].find_all("tr")  # Get all rows
 
         # Parse non-empty table body
         if len(table_rows) > 0:
-
-            print(len(table_rows))
-
-            table = []  # 2D list to be turned into a DataFrame
-
+            table = []
             for html_row in table_rows:  # For each row
-
                 row = []  # List for each row
-
                 for cell in html_row.find_all("td"):  # Get all cells
                     row.append(cell.text)  # Parse cell text
-
                 table.append(row)  # Add row to list of rows
-
-            # Create a dataframe from the list of rows
-            service_df = pd.DataFrame(
+            service_df = pd.DataFrame(  # Create a dataframe from the list of rows
                 table, columns=["Status", "Email", "Phone", "Livevox Login"]
             )
-            service_df["Service"] = service  # Add the current service as a column
-
-            # Add to total df
-            df = df.append(service_df)
-
-# Upload to Civis
-# fut = civis.io.dataframe_to_civis(df=df,database="Dover",
-#                                   table="states_co_reporting.current_thrutalk_dialers",
-#                                   client=client,
-#                                   existing_table_rows="drop",
-#                                   headers=True)
-# print(fut.result())c
-
-# import pdb; pdb.set_trace()
-gc = gs.service_account(filename="thrutalk-creds.json")
-ws = gc.open("test").worksheet("Sheet1")
-# ws.share("arvinddd2003@gmail.com", perm_type="user", role="writer")
-# import pdb
-
-# pdb.set_trace()
-
-current_time = datetime.datetime.now().replace(microsecond=0)
-columns = df.columns.to_list()
-bookmark = {}
-
-for idx, val in enumerate(columns):
-    if idx == 0:
-        bookmark[val] = "Time run: "
-    elif idx == 1:
-        bookmark[val] = str(current_time)
+            service_df["Service"] = service
+        else:
+            service_df = pd.DataFrame(
+                columns=["Status", "Email", "Phone", "Livevox Login"]
+            )
+            service_df["Service"] = service
+        return service_df
     else:
-        bookmark[val] = ""
+        raise Exception
 
-new_data = (
-    pd.DataFrame(ws.get_all_records())
-    .append(pd.Series(), ignore_index=True)
-    .append(bookmark, ignore_index=True)
-    .fillna("")
-    .append(df)
-)
 
-ws.update([new_data.columns.values.tolist()] + new_data.fillna("").values.tolist())
+def update_worksheet(ws, df):
+
+    current_time = datetime.datetime.now().replace(microsecond=0) + datetime.timedelta(
+        hours=-4
+    )
+    columns = df.columns.to_list()
+    bookmark = {}
+
+    for idx, val in enumerate(columns):
+        if idx == 1:
+            bookmark[val] = "Time run: "
+        elif idx == 2:
+            bookmark[val] = str(current_time) + " EST"
+        else:
+            bookmark[val] = ""
+
+    new_data = (
+        pd.DataFrame(ws.get_all_records())
+        .append(pd.Series(), ignore_index=True)
+        .append(bookmark, ignore_index=True)
+        .fillna("")
+        .append(df)
+    )
+
+    ws.update([new_data.columns.values.tolist()] + new_data.fillna("").values.tolist())
+
+
+# Create an empty df, this is where we will populate data from the agent logs
+cols = ["Status", "Email", "Phone", "Livevox Login", "Service"]
+df = pd.DataFrame(columns=cols)
+gc = gs.service_account(filename="thrutalk-creds.json")
+
+for service in services.keys():
+    service_df = get_agent_status_data(service)
+
+    current_date = str((datetime.datetime.now() + datetime.timedelta(hours=-4)).date())
+    sh = gc.open(services[service])
+    try:
+        ws = sh.worksheet(current_date)
+    except WorksheetNotFound:
+        ws = sh.add_worksheet(current_date, rows=100, cols=20)
+    update_worksheet(ws, service_df)
+
+    # ws.share("arvinddd2003@gmail.com", perm_type="user", role="writer")
 driver.quit()
